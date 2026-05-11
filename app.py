85807571ceff3dd6668fa22201d8cd7451c28bf9
Ubuntu <ubuntu@devops.home>
2026-05-11 13:58:57 +0200

    Add HTTP server version of app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello CI/CD')
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from DevOps CI/CD HTTP server!")
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+server.serve_forever()
+
