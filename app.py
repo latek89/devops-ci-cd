e829df167364212b258265be1fc1275362075319
latek89 <latek89@gmail.com>
2026-05-13 17:57:17 +0200

    Enable atomic deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from DevOps CI/CD HTTP server!")
+        self.wfile.write(b"Hello from DevOps CI/CD HTTP Server!")
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
 server.serve_forever()
